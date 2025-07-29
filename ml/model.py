075d5a9a60c997a3c60b589e787089c122ecaa5e
kovacswalter <lionp@LAPTOP-1682T332.localdomain>
2025-07-29 15:22:26 -0600

    changed from logistic regression to randomforestclassifier

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,7 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 
 # Optional: implement hyperparameter tuning.
@@ -20,7 +20,7 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    model = LogisticRegression()
+    model = RandomForestClassifier()
 
     model.fit(X_train,y_train)
 
