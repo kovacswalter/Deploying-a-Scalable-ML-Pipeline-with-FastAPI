8af579b79855b66aad102e3d1bfd6ad2d54b287a
kovacswalter <lionp@LAPTOP-1682T332.localdomain>
2025-07-30 12:42:59 -0600

    pytest of code ran

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,38 @@
 import pytest
+import pickle
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import train_model as tm
+import os
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_length_data_pre_postsplit():
     """
-    # add description for the first test
+    # Tests that the length of data remains the same after the split
     """
-    # Your code here
-    pass
+    data_length = len(tm.data)
+    split_len = len(tm.train)+len(tm.test)
+    assert data_length ==split_len, f"Expected {data_length}, but got {split_len}"
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model_type():
     """
-    # add description for the second test
+    Tests that model has the RandomForestClassifier type.
     """
-    # Your code here
-    pass
-
+    with open(tm.model_path, "rb") as f:
+        model = pickle.load(f)
+    assert isinstance(model,RandomForestClassifier), "Model issues"
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_metric_size():
     """
-    # add description for the third test
+    Test that precision, recall, and F1 score are within the range [0, 1].
     """
-    # Your code here
-    pass
+    assert tm.p <= 1
+    assert tm.r <= 1
+    assert tm.fb <= 1
+
+
+   
